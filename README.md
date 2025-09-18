a3fb55efc07372d2900dc3e51e38d5f72b29eaeb
felixngfender <ngthinh302@gmail.com>
2025-09-18 09:31:01 -0400

    broken: rust is hard

--- README.md
+++ README.md
@@ -44,6 +44,12 @@ a.grad = \frac{dc}{da} = ...\\
 b.grad = \frac{db}{da} = ...\\
 $$
 
+Backprop starts by seeding the output node's gradient to 1.0, then build out the
+computation graph in topological order, and then applying the chain rule to
+compute gradients for each node. The topo sort is important because it ensures
+that when we compute the gradient for a node, all of its children have already
+been computed.
+
 #### The chain rule
 
 [Wikipedia](https://en.wikipedia.org/wiki/Chain_rule)
@@ -52,3 +58,35 @@ $$
 
 Biggy arrays of scalars, packed together for efficiency when training on GPUs.
 Not useful for demonstrating backprop, so we only support scalars in darggrad.
+
+#### Neurons
+
+![Neuron](media/neuron.png)
+
+The dot product reveals how much two vectors point in the same direction. If the
+dot product is zero, the vectors are perpendicular.
+
+Multiple dot products (converting vector x vector to a scalar value) of inputs
+and weights (synaptic strengths of the input path) go into the weighted sum,
+offset by a bias ("trigger happiness" of a neuron), and put through an
+activation function. The activation function is typically a squashing function
+that limits the output to a certain range, such as between 0 and 1 (sigmoid) or
+between -1 and 1 (tanh). This helps to introduce non-linearity into the model,
+allowing it to learn more complex patterns. The loss function measures how far
+the neuron's output is from the desired output and is applied to the output
+during training.
+
+In a neural network, there are many neurons like so organized in layers.
+
+When applied to the context of backprop, we are most interested in how the loss
+is affected by the neuron's weights.
+
+#### Design decision tidbits
+
+Why wrap `Value` in shared and mutable smart pointer.
+
+![Why wrap `Value` in shared and mutable smart pointers](media/value-data-type.png)
+
+Choosing between `Box`, `Arc`, `Rc`, `Cell`, `RefCell`, `Mutex`.
+
+![Choosing between `Box`, `Arc`, `Rc`, `Cell`, `RefCell`, `Mutex`](media/box_arc_rc_cell_refcell_mutex.png)
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 use std::{
+    cell::RefCell,
+    collections::HashSet,
     fmt::{self, Display},
-    ops::{Add, Mul},
+    ops::{Add, Div, Mul},
+    rc::Rc,
 };
 
-#[derive(Clone, Debug)]
+use ordered_float::NotNan;
+
+#[derive(PartialEq, Eq, Hash)]
 enum Op {
     Add { lhs: Value, rhs: Value },
     Mul { lhs: Value, rhs: Value },
+    Div { lhs: Value, rhs: Value },
+    Tanh { input: Value },
 }
 
 impl Op {
-    fn children(&mut self) -> impl Iterator<Item = &mut Value> {
+    fn inputs(&self) -> impl Iterator<Item = &Value> {
         match self {
-            Op::Add { lhs, rhs } | Op::Mul { lhs, rhs } => vec![lhs, rhs].into_iter(),
+            Op::Add { lhs, rhs } | Op::Mul { lhs, rhs } | Op::Div { lhs, rhs } => {
+                vec![lhs, rhs].into_iter()
+            }
+            Op::Tanh { input } => vec![input].into_iter(),
+        }
+    }
+
+    fn inputs_mut(&mut self) -> impl Iterator<Item = &mut Value> {
+        match self {
+            Op::Add { lhs, rhs } | Op::Mul { lhs, rhs } | Op::Div { lhs, rhs } => {
+                vec![lhs, rhs].into_iter()
+            }
+            Op::Tanh { input } => vec![input].into_iter(),
         }
     }
 }
@@ -22,53 +41,119 @@ impl Display for Op {
         match self {
             Op::Add { .. } => write!(f, "+"),
             Op::Mul { .. } => write!(f, "*"),
+            Op::Div { .. } => write!(f, "/"),
+            Op::Tanh { .. } => write!(f, "tanh"),
         }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(PartialEq, Eq, Clone)]
 struct Value {
-    data: f32,
-    op: Option<Box<Op>>,
-    // gradient relative to the first caller of `backward()`
-    grad: f32,
+    /// Allows a `Value` to be shared and mutated across multiple owners, essentially allowing
+    /// construction of a DAG computation graph (a `Value` may contribute to more than 1 output).
+    inner: Rc<RefCell<ValueInner>>,
+}
+
+impl std::hash::Hash for Value {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.inner.borrow().hash(state);
+    }
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct ValueInner {
+    op: Option<Op>,
+    data: NotNan<f32>,
+    /// gradient relative to the first caller of `backward()`
+    grad: NotNan<f32>,
 }
 
 impl Value {
     pub fn new(data: f32) -> Self {
-        Value {
-            data,
-            op: None,
-            grad: 0.0,
+        Self {
+            inner: Rc::new(RefCell::new(ValueInner {
+                data: data.try_into().unwrap(),
+                op: None,
+                grad: NotNan::new(0.0).unwrap(),
+            })),
         }
     }
 
     fn _backward(&mut self) {
         // assume self.grad has been initialized
-        if let Some(op) = &mut self.op {
-            match op.as_mut() {
+        if let Some(op) = &mut self.inner.borrow_mut().op {
+            match op {
                 Op::Add { lhs, rhs } => {
                     // local gradients only
-                    lhs.grad = 1.0;
-                    rhs.grad = 1.0;
+                    // x
+                    lhs.inner.borrow_mut().grad += NotNan::new(1.0).unwrap();
+                    // y
+                    rhs.inner.borrow_mut().grad += NotNan::new(1.0).unwrap();
                 }
                 Op::Mul { lhs, rhs } => {
-                    lhs.grad = rhs.data;
-                    rhs.grad = lhs.data;
+                    lhs.inner.borrow_mut().grad += rhs.inner.borrow().data;
+                    rhs.inner.borrow_mut().grad += lhs.inner.borrow().data;
+                }
+                Op::Div { lhs, rhs } => {
+                    // c = a/b
+                    // dc/da = 1/b
+                    // dc/db = -a/(b^2)
+                    lhs.inner.borrow_mut().grad +=
+                        NotNan::new(1.0).unwrap() / rhs.inner.borrow().data;
+                    rhs.inner.borrow_mut().grad +=
+                        NotNan::new(-lhs.inner.borrow().data / (rhs.inner.borrow().data.powi(2)))
+                            .unwrap();
+                }
+                Op::Tanh { input } => {
+                    // y = tanhx
+                    // dy/dx = 1 - y^2
+                    input.inner.borrow_mut().grad +=
+                        NotNan::new(1.0 - self.inner.borrow().data.powi(2)).unwrap();
                 }
             }
-            op.children().for_each(|child| {
+            op.inputs_mut().for_each(|input| {
                 // chain rule
-                child.grad *= self.grad;
-                child._backward();
+                input.inner.borrow_mut().grad *= self.inner.borrow().grad;
             });
         }
     }
 
+    fn topo(&self) -> impl DoubleEndedIterator<Item = Value> {
+        // TODO: perf: less clones
+        let mut topo = vec![];
+        let mut visited = HashSet::new();
+        let mut stack = vec![self.clone()];
+        while let Some(node) = stack.pop() {
+            if !visited.contains(&node) {
+                visited.insert(node.clone());
+                if let Some(op) = &node.inner.borrow().op {
+                    for input in op.inputs() {
+                        stack.push(input.clone());
+                    }
+                }
+                topo.push(node);
+            }
+        }
+        topo.into_iter()
+    }
+
     pub fn backward(&mut self) {
+        // topo sort to ensure that when we compute gradient for a node, all gradients of its children have
+        // already been computed
         // mark the top of the computation graph
-        self.grad = 1.0;
-        self._backward();
+        self.inner.borrow_mut().grad = NotNan::new(1.0).unwrap();
+        self.topo().rev().for_each(|mut node| {
+            node._backward();
+        });
+    }
+
+    fn tanh(self) -> Self {
+        let mut res = Self::new(((2.0 * self.data).exp() - 1.0) / ((2.0 * self.data).exp() + 1.0));
+        res.op = Op::Tanh {
+            input: Rc::new(RefCell::new(self)),
+        }
+        .into();
+        res
     }
 }
 
@@ -83,7 +168,8 @@ impl Display for Value {
                 write!(f, "    ")?; // four spaces per indent level
             }
 
-            match &value.op {
+            let innner = value.inner.borrow();
+            match inner.op {
                 Some(op) => writeln!(
                     f,
                     "Value(data={:.4}, grad={:.4}, op={})",
@@ -93,10 +179,13 @@ impl Display for Value {
             }
 
             if let Some(op) = &value.op {
-                match &**op {
-                    Op::Add { lhs, rhs } | Op::Mul { lhs, rhs } => {
+                match op {
+                    Op::Add { lhs, rhs } | Op::Mul { lhs, rhs } | Op::Div { lhs, rhs } => {
                         fmt_with_indent(lhs, f, indent + 1)?;
                         fmt_with_indent(rhs, f, indent + 1)?;
+                    }
+                    Op::Tanh { input } => {
+                        fmt_with_indent(input, f, indent + 1)?;
                     }
                 }
             }
@@ -113,7 +202,11 @@ impl Add for Value {
 
     fn add(self, rhs: Self) -> Self::Output {
         let mut res = Self::new(self.data + rhs.data);
-        res.op = Box::new(Op::Add { lhs: self, rhs }).into();
+        res.op = Op::Add {
+            lhs: Rc::new(RefCell::new(self)),
+            rhs: Rc::new(RefCell::new(rhs)),
+        }
+        .into();
         res
     }
 }
@@ -123,37 +216,46 @@ impl Mul for Value {
 
     fn mul(self, rhs: Self) -> Self::Output {
         let mut res = Self::new(self.data * rhs.data);
-        res.op = Box::new(Op::Mul { lhs: self, rhs }).into();
+        res.op = Op::Mul {
+            lhs: Rc::new(RefCell::new(self)),
+            rhs: Rc::new(RefCell::new(rhs)),
+        }
+        .into();
+        res
+    }
+}
+
+impl Div for Value {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let mut res = Self::new(self.data / rhs.data);
+        res.op = Op::Div {
+            lhs: Rc::new(RefCell::new(self)),
+            rhs: Rc::new(RefCell::new(rhs)),
+        }
+        .into();
         res
     }
 }
 
 fn main() {
-    let a = Value::new(2.0);
-    let b = Value::new(-3.0);
-    let c = Value::new(10.0);
-    let e = a * b;
-    let d = e + c;
-    let f = Value::new(-2.0);
-    let mut l = d * f;
-    l.backward();
-    // dl/df = d = 4
-    // dl/dd = f = -2
-    //
-    // dl/de = dl/dd * dd/de = -2 * 1 = -2
-    // dl/dc = dl/dd * dd/dc = -2 * 1 = -2
-    //
-    // dl/da = dl/de * de/da = -2 * b = -2 * -3 = 6
-    // dl/db = dl/de * de/db = -2 * a = -2 * 2 = -4
-    //
-    // Value(data=-8.0000, grad=1.0000, op=*)
-    //     Value(data=4.0000, grad=-2.0000, op=+)
-    //         Value(data=-6.0000, grad=-2.0000, op=*)
-    //             Value(data=2.0000, grad=6.0000)
-    //             Value(data=-3.0000, grad=-4.0000)
-    //         Value(data=10.0000, grad=-2.0000)
-    //     Value(data=-2.0000, grad=4.0000)
-    println!("{}", l);
+    // inputs
+    let x1 = Value::new(2.0);
+    let x2 = Value::new(0.0);
+    // weights
+    let w1 = Value::new(-3.0);
+    let w2 = Value::new(1.0);
+    // bias
+    let b = Value::new(6.881_373_587_019_543_2);
+    // hidden layers
+    let w1x1 = x1.clone() * w1;
+    let w2x2 = x2 * w2;
+    let w1x1w2x2 = w1x1 + w2x2;
+    let n = w1x1w2x2 + b;
+    let mut o = n.tanh();
+    o.backward();
+    println!("{}", o);
 }
 
 #[cfg(test)]
@@ -172,7 +274,7 @@ mod tests {
         if let Some(op) = &c.op {
             match &**op {
                 Op::Add { lhs, rhs } => {
-                    assert_eq!(lhs.grad, 1.0);
+                    assert_eq!(lhs.borrow().grad, 1.0);
                     assert_eq!(rhs.grad, 1.0);
                 }
                 _ => panic!("expected add operation"),
